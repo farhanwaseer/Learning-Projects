769ca6703a7cfc6ab24110fac360446f7459ba2a
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-26 22:08:00 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -12,7 +12,7 @@ const Book = () => {
 }
 
 const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/51pu2NIKVvL._AC_SX184_.jpg" alt=""/>
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg" alt=""/>
 }
 
 
