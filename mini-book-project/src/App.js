e614016b9314358675ab7b33bacbfd76f31c3b4e
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-26 22:09:59 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -15,5 +15,6 @@ const Image = () => {
   return <img src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg" alt=""/>
 }
 
+const Title = () => <h1>The Last Thing He Told Me : A Novel</h1>
 
 export default App;
