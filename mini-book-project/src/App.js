75bb6107188a8140ec758a0778531bc5f856deda
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-26 22:03:24 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -3,17 +3,16 @@ function App() {
   return (
     <section >
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
     </section>
   );
 }
 
 const Book = () => {
   return <article> This is Book-List  </article>;
+}
+
+const Image = () => {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/51pu2NIKVvL._AC_SX184_.jpg" alt=""/>
 }
 
 
