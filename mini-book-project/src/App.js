dc96dbb80a29f70fc23f5864450281864c5b5cf1
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-26 22:11:11 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -1,20 +1,28 @@
-
 function App() {
   return (
-    <section >
-    <Book />
+    <section>
+      <Book />
     </section>
   );
 }
 
 const Book = () => {
-  return <article> <Image /></article>;
-}
+  return (
+    <article>
+      <Image />
+    </article>
+  );
+};
 
 const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg" alt=""/>
-}
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg"
+      alt=""
+    />
+  );
+};
 
-const Title = () => <h1>The Last Thing He Told Me : A Novel</h1>
+const Title = () => <h1>The Last Thing He Told Me : A Novel</h1>;
 
 export default App;
