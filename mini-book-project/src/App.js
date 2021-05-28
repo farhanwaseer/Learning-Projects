87092d65db53a9528703c4d6ba3033e023176f5e
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-28 13:08:53 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -21,23 +21,18 @@ function App() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+       <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg"
+      alt=""
+    />
+       <h1>The Last Thing He Told Me : A Novel</h1>
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg"
-      alt=""
-    />
-  );
-};
 
-const Title = () => <h1>The Last Thing He Told Me : A Novel</h1>;
+
+
 const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Laura Dave</h4>
 
 export default App;
