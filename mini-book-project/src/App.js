efe14ccd6d8aa06af371a220f84fe734ad3a089e
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-30 22:11:58 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -5,22 +5,28 @@ import "./App.css";
 const firstBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg",
   title: "The Last Thing He Told Me: A Novel",
-  author: "Laura Dave"
-}
+  author: "Laura Dave",
+};
 
 const secondBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/51U2mcE5iXL._AC_SX184_.jpg",
   title: "Second Place: A Novel ",
-  author: "Rachel Cusk"
-}
+  author: "Rachel Cusk",
+};
 
 function App() {
   return (
     <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
-      
-     
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
@@ -28,17 +34,11 @@ function App() {
 const Book = (props) => {
   return (
     <article className="book">
-       <img
-      src={props.img}
-      alt=""
-    />
-       <h1>{props.title}</h1>
-       <h4 >{props.author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-
-
 
 export default App;
