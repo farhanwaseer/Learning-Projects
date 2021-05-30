e791e9e8dcdf9eb54c6bd74ece8fce837f2ec28f
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-30 23:02:24 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -1,38 +1,33 @@
 // CSS
 import "./App.css";
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg",
-  title: "The Last Thing He Told Me: A Novel",
-  author: "Laura Dave",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._AC_SX184_.jpg",
+    title: "The Last Thing He Told Me: A Novel",
+    author: "Laura Dave",
+  },
 
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51U2mcE5iXL._AC_SX184_.jpg",
-  title: "Second Place: A Novel ",
-  author: "Rachel Cusk",
-};
-
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51U2mcE5iXL._AC_SX184_.jpg",
+    title: "Second Place: A Novel ",
+    author: "Rachel Cusk",
+  }
+];
 function App() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {
+        books.map((book)=> {
+          return <Book book={book} ></Book>
+        })
+      }
     </section>
   );
 }
 
 const Book = (props) => {
-
-  const {img, title, author} = props;
+  const { img, title, author } = props;
 
   return (
     <article className="book">
