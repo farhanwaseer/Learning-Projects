c63d7270f9fb56e5e8886799c12aaeaf0dc47799
farhanwaseer <farhanaliwaseer@gmail.com>
2021-05-30 22:26:20 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -44,7 +44,7 @@ const Book = (props) => {
       <img src={props.img} alt="" />
       <h1>{props.title}</h1>
       <h4>{props.author}</h4>
-      {Children}
+      {props.children}
     </article>
   );
 };
