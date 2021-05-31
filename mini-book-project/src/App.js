e19051508967f849d26e884ad1a1170bb786dc2e
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2021-05-31 07:43:19 -0700

    ... ....

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -12,6 +12,20 @@ const books = [
   {
     id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/51U2mcE5iXL._AC_SX184_.jpg",
+    title: "The Four Agreements: A Practical Guide to Person ",
+    author: "Don Miguel Ruiz",
+  },
+
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51FcSSKpJBL._AC_SX184_.jpg",
+    title: "Second Place: A Novel ",
+    author: "Rachel Cusk",
+  },
+
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51U2mcE5iXL._AC_SX184_.jpg",
     title: "Second Place: A Novel ",
     author: "Rachel Cusk",
   }
