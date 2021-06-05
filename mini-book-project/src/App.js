44386204322b1bcdcb2042049d3ee24f53e2e077
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-05 23:58:43 +0500

    .

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -1,5 +1,5 @@
-import {books} from "./books";
-
+import {books } from "./books";
+import {Book} from "./Book";
 
 // CSS
 import "./App.css";
@@ -22,17 +22,5 @@ const handler = () => {
 };
 
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
-
-  return (
-    <article className="book">
-      <img src={img} alt="books" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button onClick={handler}>example button</button>
-    </article>
-  );
-};
 
 export default App;
