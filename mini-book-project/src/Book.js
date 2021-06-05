d926a01c4c5cbb7efac19bd616123c53e08ccd1b
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-05 23:52:57 +0500

    .

---
+++ mini-book-project/src/Book.js
@@ -0,0 +1,13 @@
+export const Book = (props) => {
+    const { img, title, author } = props.book;
+  
+    return (
+      <article className="book">
+        <img src={img} alt="books" />
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+        <button onClick={handler}>example button</button>
+      </article>
+    );
+  };
+  
