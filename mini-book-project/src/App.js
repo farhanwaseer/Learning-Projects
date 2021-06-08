885593cb2062cf60c6bd0fe126e6bcb325427f9f
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-08 22:28:53 +0500

    ...

--- mini-book-project/src/App.js
+++ mini-book-project/src/App.js
@@ -1,25 +1,18 @@
-import {books } from "./books";
-import {Book} from "./Book";
+import { books } from "./books";
+import { Book } from "./Book";
 
 // CSS
 import "./App.css";
 
-
 function App() {
   return (
     <section className="booklist">
-      {
-        books.map((book) => {
-          return <Book key={books.id} book={book}/>
-        })
-      }
+      {books.map((book) => {
+        return <Book key={books.id} book={book} />;
+      })}
     </section>
   );
 }
-
-const handler = () => {
-  alert("helloWorld")
-};
 
 
 
