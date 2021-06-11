67f963ae1ddbf0e948f1122688d9ee8f3373500c
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-12 03:01:30 +0500

    ,

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section>
+        <h3>0 birthday today</h3>
+      </section>
+    </main>
+  )
 }
 
 export default App;
