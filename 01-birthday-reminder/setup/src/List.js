2d602c3466c6486342e149a52d9f14d18248683c
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-12 02:23:42 +0500

    .

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-const List = ({ people }) => {
+const List = ({people}) => {
   return (
     <>
       {people.map((person) => {
         const { id, image, age, name } = person;
-        return <article key={id} className="person"></article>;
+        return (
+          <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age}</p>
+          </div>
+        </article>
+        );
       })}
     </>
   );
