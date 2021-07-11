8c49fcd539e457686567d9369a034b302ee5a8e1
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-11 21:26:24 +0500

    .

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -4,7 +4,9 @@ import List from './List';
 function App() {
   return (
     <main>
-      
+      <section className='container'>
+        <h3>0 birthdays today</h3>
+      </section>
     </main>
   );
 }
