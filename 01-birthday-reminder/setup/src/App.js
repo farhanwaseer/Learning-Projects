84c96107c100215f4b3940505a33fd42520cad8d
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-12 02:05:31 +0500

    .

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
         <List people={people.length}/>
         <button onClick={()=> console.log('click me')}>clear all</button>
       </section>
