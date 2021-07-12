8ba2a3dadcbc4d57e6f4b13b4b6ae01542130399
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-12 23:46:25 +0500

    ,

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -10,14 +10,21 @@ function App() {
 
   const fetchTours = async () => {
     setLoading(true);
-    const response = await fetch(url);
+    try {
+      const response = await fetch(url);
     const tours = await response.json();
-    console.log(tours);
+    setLoading(false);
+    setTours(tours);    
+    } 
+    catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     fetchTours();
-    }, [])
+  }, []);
 
   if (loading) {
     return (
