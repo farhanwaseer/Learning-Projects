13496092e3c725181e75a6123d26a85d7bcd5894
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-12 23:28:21 +0500

    .

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [tours,setTours] = useState([]);
-  if(loading){
-    return <main>
-      <Loading />
-    </main>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
   }
-  return <h2>Tours Project Setup</h2>
+  return <h2>Tours Project Setup</h2>;
 }
 
-export default App
+export default App;
