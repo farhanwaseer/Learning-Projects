23d73b4ba29d20a247ca2a8165dcb3848adf648f
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-16 23:36:49 +0500

    .

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -7,7 +7,6 @@ function App() {
       <section className="contanier">
         <div className="title">
           <h2>our Review</h2>
-          <Review />
           <div className="underline"></div>
         </div>
       </section>
