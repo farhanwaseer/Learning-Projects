5a67b896e20405dd80fbbb069e48fd063367dc83
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-16 23:25:42 +0500

    .

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Review from './Review';
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <main>
+    <div className="contanier">
+      <div className="title">
+      <h2>our Review</h2>
+      </div>
+    </div>
+  </main>
 }
 
 export default App;
