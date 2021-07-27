9d8c5cc0d766e8a36eaada22148073480e72dd11
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-28 00:46:57 +0500

    .

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -3,12 +3,19 @@ import React from "react";
 const Categories = ({ filterItems }) => {
   return (
     <div className="btn-container">
+          <button
+        className="filter-btn"
+        onClick={()=> filterItems('all')}       
+      >
+        all
+      </button>
       <button
         className="filter-btn"
         onClick={()=> filterItems('breakfast')}       
       >
         breakfast
       </button>
+
     </div>
   );
 };
