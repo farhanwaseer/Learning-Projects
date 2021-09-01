79a26ec3eec7d502ee0f3e67218232027b430091
farhanwaseer <farhanaliwaseer@gmail.com>
2021-09-01 23:38:32 +0500

    .

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   const [count,setCount] = useState(0);
-  const [text,setText] = useState('');
+  const [text,setText] = useState([ ]);
   
   return (
-  <h2>lorem ipsum project setup</h2>
+       <section className='section-center'>
+          <h2>lorem ipsum project setup</h2>
+       </section>
     )
 }
 
