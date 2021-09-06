b3411ab2b69b52be5f940122ad0ca10359f79939
farhanwaseer <farhanaliwaseer@gmail.com>
2021-09-07 00:03:08 +0500

    /

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items,removeItem }) => {
+const List = ({ items, removeItem }) => {
   return (
     <div className="grocery-list">
       {items.map((item) => {
         const { id, title } = item;
-     
+
         return (
           <article key={id} className="grocery-item">
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button type='button' className='edit-btn'>
-                <FaEdit/>
+              <button type="button" className="edit-btn">
+                <FaEdit />
               </button>
-              <button type='button' className='delete-btn' onClick={() =>removeItem(id)}>
-                <FaTrash/>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
               </button>
             </div>
           </article>
