0b9e982169d40a25fb16f90fa59f85ed8591d047
farhanwaseer <farhanaliwaseer@gmail.com>
2021-09-07 00:19:55 +0500

    .

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -40,6 +40,12 @@ function App() {
     showAlert(true, "danger", "item removed");
     setList(list.filter((item) => item.id !== id));
   };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  }
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -61,7 +67,7 @@ function App() {
 
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem}/>
           <button className="clear-btn" onClick={clearList}>
             {" "}
             clear items
